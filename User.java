bfe9acbe443902e299ba9e8d1e97163284e1fc11
Vanea B <75329633+iCount@users.noreply.github.com>
2022-09-20 18:01:54 +0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,39 @@
+package com.xzy;
+
+// Пользователь
+public class User {
+    int id;
+    String password;
+
+    @Override
+    public boolean equals(Object obj) {
+        User u = (User) obj;
+        return this.id == u.id;
+    }
+
+    public User(int id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "{Аккаунт:" + id + "Пароль:" + password + "}";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassward() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
